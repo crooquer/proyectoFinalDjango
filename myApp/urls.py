e107cc671c43dadbfe9a564f38f5f2b2c2e90eab
crooquer <josecrooquer@gmail.com>
2023-12-03 18:31:25 -0300

    tercer commit

--- myApp/urls.py
+++ myApp/urls.py
@@ -3,5 +3,9 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('about/', views.about),
+    path('experiencia/', views.experiencia),
+    path('servicios/', views.servicios),
+    path('contacto/', views.contacto)
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -8,3 +8,15 @@ def index(request):
     return render(request, 'index.html', {
         'title': title
     })
+
+def about(request):
+    return render(request, 'about.html')
+
+def experiencia(request):
+    return render(request, 'experiencia.html')
+
+def servicios(request):
+    return render(request, 'servicios.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
